a5d1030ce213d85268b43279c6e790d26b3a92a8
Wilson-Benedicta <2425404324@live.gctu.edu.gh>
2026-05-31 12:26:52 +0100

    Update and rename initials.py to BWE.py

---
+++ BWE.py
@@ -0,0 +1,35 @@
+# File: BWE.py
+# Description: Print out my initials
+# Assignment Number: 2
+#
+# Name: Benedicta Wilson Eyram
+# STUDENT ID:  2425404324
+# Email: 2425404324@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor,Benedicta Wilson Eyram,this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+def main():
+ # Print initials BWE:as large 12x10 block letters
+    # made from their own capital letters and periods.
+
+    print()
+    print("...BWE")
+    print()
+    print("...BBBBBBBBBB..........WW.........WW........EEEEEEEEEEEE...")
+    print("...BB.......BB.........WW.........WW........EE.............")
+    print("...BB.......BB.........WW.........WW........EE.............")
+    print("...BB.....BBB..........WW.........WW........EE.............")
+    print("...BBBBBBBBB...........WW....W....WW........EEEEEEEEEEE....")
+    print("...BB......BBB.........WW...WWW...WW........EEEEEEEEEEE....")
+    print("...BB.......BB.........WW.W.WWW.W.WW........EE.............")
+    print("...BB.......BB.........WWW.......WWW........EE.............")
+    print("...BB.......BB....**...WW.........WW...**...EE...........**")
+    print("...BBBBBBBBBB.....**...W...........W...**...EEEEEEEEEEEE.**")
+    print()
+    print()
+main()
+
+
+main()
