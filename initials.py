8c6006e69cc52e3ecdac368be2bef0299afed78b
Wilson-Benedicta <2425404324@live.gctu.edu.gh>
2026-05-31 23:02:00 +0100

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,20 +1,18 @@
 # File: initials.py
 # Description: Printing out my initials.
-# Assignment Number: 2
+# Assignment Number: 1
 #
 # Name: Benedicta Wilson Eyram
 # STUDENT ID:  2425404324
 # Email: 2425404324@live.gctu.edu.gh
-# Grader: Augustus Buckman
+# Grader: Augustus
 #
 # On my honor, Benedicta Wilson Eyram, this programming assignment is my own work
 # and I have not provided this code to any other student.
 
 
 def main():
-    # Print initials BWE as large block letters made from their own
-    # capital letters and periods, with large asterisk periods after each.
-
+    # Print initials BWE as large block letters made from their own letters
     print()
     print("...BWE")
     print()
